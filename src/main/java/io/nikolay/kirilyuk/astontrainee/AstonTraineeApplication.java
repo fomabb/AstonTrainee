0354371f992a0c1da4796a1418b0fc3b4df5ba75
fomabb <fomabb@yandex.by>
2024-07-10 11:58:41 +0300

    initial commit

---
+++ src/main/java/io/nikolay/kirilyuk/astontrainee/AstonTraineeApplication.java
@@ -0,0 +1,13 @@
+package io.nikolay.kirilyuk.astontrainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonTraineeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstonTraineeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nikolay/kirilyuk/astontrainee/AstonTraineeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nikolay.kirilyuk.astontrainee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonTraineeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
